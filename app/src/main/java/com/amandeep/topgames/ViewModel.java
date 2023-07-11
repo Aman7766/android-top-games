6f68d61bd9923c77e20dc0fb83926fda0fb8c7f3
Amandeep <amankumar283@gmail.com>
2023-07-11 14:25:12 -0400

    ViewModel Java Added

---
+++ app/src/main/java/com/amandeep/topgames/ViewModel.java
@@ -0,0 +1,16 @@
+package com.amandeep.topgames;
+
+public class ViewModel {
+
+    private int image;
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+    public int getImage() {
+        return image;
+    }
+
+}
